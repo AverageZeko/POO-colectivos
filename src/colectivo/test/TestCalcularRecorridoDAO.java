9134c03041697b57a4837956e21ecf78bfa4a14e
AverageZeko <lizandro.e.marino@gmail.comgit config --global user.email>
2025-11-16 13:12:19 -0300

    Arreglo de archivos de test

--- src/colectivo/test/TestCalcularRecorridoDAO.java
+++ src/colectivo/test/TestCalcularRecorridoDAO.java
@@ -1,6 +1,6 @@
 package colectivo.test;
 
-import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
--- src/colectivo/test/TestCalcularRecorridoDAO_AZL.java
+++ src/colectivo/test/TestCalcularRecorridoDAO_AZL.java
@@ -15,6 +15,7 @@ import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import colectivo.configuracion.ConfigGlobal;
 import colectivo.dao.LineaDAO;
 import colectivo.dao.ParadaDAO;
 import colectivo.dao.TramoDAO;
@@ -23,8 +24,6 @@ import colectivo.modelo.Linea;
 import colectivo.modelo.Parada;
 import colectivo.modelo.Recorrido;
 import colectivo.modelo.Tramo;
-import colectivo.servicio.SchemaServicio;
-import colectivo.servicio.SchemaServicioImplementacion;
 import colectivo.util.Factory;
 
 class TestCalcularRecorridoDAO_AZL {
@@ -37,12 +36,12 @@ class TestCalcularRecorridoDAO_AZL {
 	private LocalTime horaLlegaParada;
 	
 	private Calculo calculo;	
-	private SchemaServicio schemaServicio;
+	private ConfigGlobal configuracion;
 
 	@BeforeEach
 	void setUp() throws Exception {
-		schemaServicio = new SchemaServicioImplementacion();
-		schemaServicio.cambiarSchema("colectivo_AZL");
+		configuracion = ConfigGlobal.getConfiguracion();
+		configuracion.cambiarCiudad("Azul");
 
 		paradas = ((ParadaDAO) Factory.getInstancia("PARADA")).buscarTodos();
 		tramos = ((TramoDAO) Factory.getInstancia("TRAMO")).buscarTodos();
